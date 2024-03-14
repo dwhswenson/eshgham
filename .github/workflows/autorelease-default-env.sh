6d13dd361b930f3b967ae8ba47157d25204eefc8
David W.H. Swenson <dwhs@hyperblazer.net>
2024-03-14 14:50:10 -0500

    update autorelease actions

--- .github/workflows/autorelease-default-env.sh
+++ .github/workflows/autorelease-default-env.sh
@@ -1,6 +1,6 @@
-# Vendored from Autorelease 0.4.0
+# Vendored from Autorelease 0.5.1
 # Update by updating Autorelease and running `autorelease vendor actions`
-INSTALL_AUTORELEASE="python -m pip install autorelease==0.4.0"
+INSTALL_AUTORELEASE="python -m pip install autorelease==0.5.1"
 if [ -f autorelease-env.sh ]; then
     source autorelease-env.sh
 fi
