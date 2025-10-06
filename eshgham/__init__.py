a41c541bf467eaaeb61d46b56d4b463239f84eee
David W.H. Swenson <dwhs@hyperblazer.net>
2025-10-06 14:53:44 -0400

    ruff

--- eshgham/__init__.py
+++ eshgham/__init__.py
@@ -13,12 +13,20 @@ import yaml
 from colorama import Fore
 
 __all__ = [
-    "Status", "Result", "Outputter", "JSONOutputter", "ColorOutputter",
-    "Harness", "get_workflow_result" "attempt_to_reactivate" "get_token",
-    "make_parser", "main", "make_json_ready"
+    "Status",
+    "Result",
+    "Outputter",
+    "JSONOutputter",
+    "ColorOutputter",
+    "Harness",
+    "get_workflow_resultattempt_to_reactivateget_token",
+    "make_parser",
+    "main",
+    "make_json_ready",
 ]
 
 from .version import short_version
+
 
 class Status(enum.Enum):
     OK = 0
@@ -47,28 +55,23 @@ class Result(typing.NamedTuple):
         start = f"https://github.com/{repo}/blob"
         end = self.workflow.path
         if not (html_url.startswith(start) and html_url.endswith(end)):
-            url = ("Unable to get actions URL for action at:\n  "
-                   + html_url)
+            url = "Unable to get actions URL for action at:\n  " + html_url
         else:
-            file = end.split('/')[-1]
+            file = end.split("/")[-1]
             url = f"https://github.com/{repo}/actions/workflows/{file}"
 
         return url
 
 
-def get_workflow_result(
-    repo: github.Repository.Repository,
-    workflow_name: str
-):
-    """Create :class:`.Result` for a single workflow.
-    """
+def get_workflow_result(repo: github.Repository.Repository, workflow_name: str):
+    """Create :class:`.Result` for a single workflow."""
     workflow = repo.get_workflow(workflow_name)
     if workflow.state != "active":
         status = Status.INACTIVATED
         last_scheduled_run = None
     else:
         try:
-            last_scheduled_run = next(iter(workflow.get_runs(event='schedule')))
+            last_scheduled_run = next(iter(workflow.get_runs(event="schedule")))
         except StopIteration:
             last_scheduled_run = None
             status = Status.NO_SCHEDULED_RUNS
@@ -83,12 +86,11 @@ def get_workflow_result(
         workflow_name=workflow_name,
         status=status,
         workflow=workflow,
-        last_scheduled_run=last_scheduled_run
+        last_scheduled_run=last_scheduled_run,
     )
 
 
-def attempt_to_reactivate(result):
-    ...
+def attempt_to_reactivate(result): ...
 
 
 def get_token(arg_token, workflow_dict):
@@ -98,8 +100,8 @@ def get_token(arg_token, workflow_dict):
     # 3. File ~/.githubtoken
     # 3. env var GITHUB_TOKEN
     token = None
-    if 'token' in workflow_dict:
-        token = workflow_dict.pop('token')
+    if "token" in workflow_dict:
+        token = workflow_dict.pop("token")
     if arg_token:
         token = arg_token
     if not token:
@@ -114,6 +116,7 @@ def get_token(arg_token, workflow_dict):
         raise ValueError("Missing 'token' parameter")
     return token
 
+
 def make_parser():
     parser = argparse.ArgumentParser(
         prog="eshgham",
@@ -121,29 +124,35 @@ def make_parser():
             "ESHGHAM: The Executive Summarizer of Health for GitHub "
             "Actions Monitoring. "
             "A dashboard for your neglected GitHub Actions "
-            f"workflows. Version {short_version}.")
+            f"workflows. Version {short_version}."
+        ),
     )
     parser.add_argument(
-        'workflows_yaml', type=str,
+        "workflows_yaml",
+        type=str,
         help=(
             "Workflows in YAML format. This is provided with repository "
             "'owner/repo_name' as a string key, and a list of workflow "
             "filenames as the value. The special key 'token' may be used "
             "for the GitHub personal access token."
-        )
+        ),
     )
     parser.add_argument(
-        '--token', type=str,
+        "--token",
+        type=str,
         help=(
             "GitHub personal access token. May also be provided using "
             "'token' as a key in the workflow YAML file, or in the "
             "environment variable `GITHUB_TOKEN`. The command argument "
             "takes precedence, followed bythe environment variable, and "
             "finally the YAML specification."
-        )
+        ),
     )
     parser.add_argument(
-        '--json', action="store_const", dest="runtype", const="json",
+        "--json",
+        action="store_const",
+        dest="runtype",
+        const="json",
         default="color",
         help=(
             "Output as JSON instead of outputting to screen. The JSON "
@@ -152,22 +161,24 @@ def make_parser():
             "has the repository name, the workflow name, and a URL. For "
             "passing/failing workflows, this URL points to the last run. "
             "For inactive workflows, the URL points to the workflow itself."
-        )
+        ),
     )
     parser.add_argument(
-        '--exit-code', type=int, default=1,
+        "--exit-code",
+        type=int,
+        default=1,
         help=(
             "Exit code to return if there are any inactive/failing "
             "workflows. Runs with no inactive/failing workflows always "
             "exit with 0."
-        )
+        ),
     )
     return parser
 
 
 class Outputter:
-    """Abstract base for output handling with the :class:`.Harness`
-    """
+    """Abstract base for output handling with the :class:`.Harness`"""
+
     def before_any(self):
         return None
 
@@ -191,21 +202,18 @@ class Outputter:
 
 
 class Harness:
-    """Generic harness to loop over all workflows in a workflow dict.
-    """
+    """Generic harness to loop over all workflows in a workflow dict."""
+
     def __init__(self, outputter: Outputter):
         self.out = outputter
 
-    def __call__(
-        self,
-        gh: github.Github,
-        workflow_dict: dict[str, list[str]]
-    ):
-        """
-        """
+    def __call__(self, gh: github.Github, workflow_dict: dict[str, list[str]]):
+        """ """
         self.out.before_any()
 
         results = []
+
+        # TODO: kick off result gathering using async; then report in serial
 
         for repo_name, workflow_list in workflow_dict.items():
             self.out.before_repo(repo_name, workflow_list)
@@ -222,8 +230,12 @@ class Harness:
             self.out.after_repo(repo_results)
             results.extend(repo_results)
         self.out.after_all(results)
-        sorted_results = {Status.OK: [], Status.INACTIVATED: [],
-                          Status.FAILED: [], Status.NO_SCHEDULED_RUNS: []}
+        sorted_results = {
+            Status.OK: [],
+            Status.INACTIVATED: [],
+            Status.FAILED: [],
+            Status.NO_SCHEDULED_RUNS: [],
+        }
         for res in results:
             sorted_results[res.status].append(res)
 
@@ -234,9 +246,9 @@ class Harness:
 def make_json_ready(grouped_results):
     def workflow_info(workflow):
         return {
-            'repo': workflow.repo,
-            'workflow_name': workflow.workflow_name,
-            'url': workflow.output_url,
+            "repo": workflow.repo,
+            "workflow_name": workflow.workflow_name,
+            "url": workflow.output_url,
         }
 
     json_ready = {
@@ -248,6 +260,7 @@ def make_json_ready(grouped_results):
 
 class JSONOutputter(Outputter):
     """Outputter for JSON output"""
+
     def with_sorted_results(self, sorted_results):
         json_ready = make_json_ready(sorted_results)
         print(json.dumps(json_ready))
@@ -255,6 +268,7 @@ class JSONOutputter(Outputter):
 
 class ColorOutputter(Outputter):
     """Outputter for colorama-based screen output"""
+
     @staticmethod
     def _wrap_status_color(text, status):
         color = {
@@ -270,7 +284,7 @@ class ColorOutputter(Outputter):
             Status.OK: "OK",
             Status.INACTIVATED: "INACTIVE",
             Status.FAILED: "FAIL",
-            Status.NO_SCHEDULED_RUNS: "NO SCHEDULED RUNS"
+            Status.NO_SCHEDULED_RUNS: "NO SCHEDULED RUNS",
         }[result.status]
         stylized = self._wrap_status_color(text, result.status)
         print(f"{result.repo}: {result.workflow_name}: {stylized}")
@@ -279,28 +293,31 @@ class ColorOutputter(Outputter):
         if inactives := sorted_results[Status.INACTIVATED]:
             print("\nLINKS TO INACTIVE WORKFLOWS")
             for result in inactives:
-                print(f"* {result.repo}:{result.workflow_name} page:\n"
-                      f"{result.output_url}")
+                print(
+                    f"* {result.repo}:{result.workflow_name} page:\n{result.output_url}"
+                )
 
         if failures := sorted_results[Status.FAILED]:
             print("\nLINKS TO FAILED RUNS")
             for result in failures:
-                print(f"* {result.repo}:{result.workflow_name} failure: "
-                      f"\n  {result.output_url}")
+                print(
+                    f"* {result.repo}:{result.workflow_name} failure: "
+                    f"\n  {result.output_url}"
+                )
 
         total = sum(len(ll) for ll in sorted_results.values())
 
         def _summary_string(sorted_results, status, text):
             return self._wrap_status_color(
-                f"{len(sorted_results[status])} {text}.",
-                status
+                f"{len(sorted_results[status])} {text}.", status
             )
 
         print(
             f"\nChecked {total} workflows. "
-            + _summary_string(sorted_results, Status.OK, "passing") + " "
-            + _summary_string(sorted_results, Status.INACTIVATED,
-                              "inactive") + " "
+            + _summary_string(sorted_results, Status.OK, "passing")
+            + " "
+            + _summary_string(sorted_results, Status.INACTIVATED, "inactive")
+            + " "
             + _summary_string(sorted_results, Status.FAILED, "failing")
         )
 
@@ -316,8 +333,8 @@ def main() -> int:
     gh = github.Github(token)
 
     outputter = {
-        'json': JSONOutputter(),
-        'color': ColorOutputter(),
+        "json": JSONOutputter(),
+        "color": ColorOutputter(),
     }[args.runtype]
 
     runner = Harness(outputter)
--- eshgham/__main__.py
+++ eshgham/__main__.py
@@ -1,8 +1,10 @@
 from eshgham import main
 
+
 def main_exe():
     """Actual entry point"""
     exit(main())
+
 
 if __name__ == "__main__":
     main_exe()
